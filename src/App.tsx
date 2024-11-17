c8ac4adb81b51502228269d6f736c640ac99a336
Peter T. Vaichus <peterthomas@Peters-MacBook-Air-2.local>
2024-11-17 13:58:56 -0500

    code

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="navbar">
+        <ul>
+          <li>Home</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
